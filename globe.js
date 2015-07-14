ab7d0572dca8575fa561e8932a0621eee5b2e95c
cherriewang <cherrie.cherry@gmail.com>
2015-07-14 15:33:51 -0400

    Adds NASA logo

--- globe.js
+++ globe.js
@@ -1,13 +1,7 @@
 /**
  * dat.globe Javascript WebGL Globe Toolkit
  * http://dataarts.github.com/dat.globe
- *
- * Copyright 2011 Data Arts Team, Google Creative Lab
- *
  * Licensed under the Apache License, Version 2.0 (the 'License');
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
  * http://www.apache.org/licenses/LICENSE-2.0
  */
 
