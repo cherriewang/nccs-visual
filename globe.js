9cddb72f3e78a2c094d145a2feacd1736f74d16e
cherriewang <cherrie.cherry@gmail.com>
2015-07-28 14:42:55 -0400

    change vid src

--- globe.js
+++ globe.js
@@ -130,7 +130,7 @@ DAT.Globe = function(container, opts) {
     video = document.createElement( 'video' );
     // video.id = 'video';
     // video.type = ' video/ogg; codecs="theora, vorbis" ';
-    video.src = "/final_webm_sq.webm";
+    video.src = "nccs-visual/final_webm_sq.webm";
     video.load(); // must call after setting/changing source
     video.play();
     
